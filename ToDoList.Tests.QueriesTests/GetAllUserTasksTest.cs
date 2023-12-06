219af502051b6516bb65c7f628f405259e0ff074
Юрій Козіцький <y.kozitski@gmail.com>
2023-12-06 15:59:27 +0200

    added queries unit tests

---
+++ ToDoList.Tests.QueriesTests/GetAllUserTasksTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Core.Models;
+using ToDoList.Core.RepositoryInterfaces;
+using ToDoList.Infrastructure.Mediator.Queries;
+
+namespace ToDoList.Tests.QueriesTests
+{
+	public class GetAllUserTasksTest
+	{
+		[Fact]
+		public void GetAllUserTasksDoesNotReturnNull()
+		{
+			//Arrange
+			var taskRepositoryMock=new Mock<ITaskRepository>();
+			taskRepositoryMock.Setup(repo => repo.GetAllUserTasksAsync("mockmail@gmail.com")).Returns(GetAllUserTasksList);
+			var getAllUserTasksHandler = new GetAllUserTasksHandler(taskRepositoryMock.Object);
+
+			//Act
+			var result = getAllUserTasksHandler.Handle;
+
+			//Assert
+			Assert.NotNull(result(new GetAllUserTasksQuery() {UserEmail= "mockmail@gmail.com" },default).Result);
+
+		}
+
+		private Task<IEnumerable<UserTask>> GetAllUserTasksList()
+		{
+			return Task.FromResult(new List<UserTask>
+			{
+				new UserTask() { Text = "mock1", TaskList = new TaskList() },
+				new UserTask() { Text = "mock2", TaskList = new TaskList() },
+				new UserTask() { Text = "mock3", TaskList = new TaskList() }
+			}.AsEnumerable());
+		}
+	}
+}
---
+++ ToDoList.Tests.QueriesTests/GetImportantTasksTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Core.Models;
+using ToDoList.Core.RepositoryInterfaces;
+using ToDoList.Infrastructure.Mediator.Queries;
+
+namespace ToDoList.Tests.QueriesTests
+{
+	public class GetImportantTasksTest
+	{
+		[Fact]
+		public void GetImportantTasksDoesNotReturnNull()
+		{
+			//Arrange
+			var taskRepositoryMock = new Mock<ITaskRepository>();
+			taskRepositoryMock.Setup(repo => repo.GetImportantTasksAsync("mockmail@gmail.com")).Returns(GetImportantUserTasksList);
+			var getImportantUserTasksHandler = new GetImportantTasksHandler(taskRepositoryMock.Object);
+
+			//Act
+			var result = getImportantUserTasksHandler.Handle;
+
+			//Assert
+			Assert.NotNull(result(new GetImportantTasksQuery() { UserEmail = "mockmail@gmail.com" }, default).Result);
+
+		}
+
+		[Fact]
+		public void GetOnlyImportantTasks()
+		{
+			//Arrange
+			var taskRepositoryMock = new Mock<ITaskRepository>();
+			taskRepositoryMock.Setup(repo => repo.GetImportantTasksAsync("mockmail@gmail.com")).Returns(GetImportantUserTasksList);
+			var getImportantUserTasksHandler = new GetImportantTasksHandler(taskRepositoryMock.Object);
+
+			//Act
+			var result = getImportantUserTasksHandler.Handle;
+
+			//Assert
+			Assert.True(result(new GetImportantTasksQuery() { UserEmail = "mockmail@gmail.com" }, default).Result.All(userTask=>userTask.IsImportant));
+
+		}
+
+		private Task<IEnumerable<UserTask>> GetImportantUserTasksList()
+		{
+			return Task.FromResult(new List<UserTask>
+			{
+				new UserTask() { Text = "mock1", IsImportant=true, TaskList = new TaskList() },
+				new UserTask() { Text = "mock2", IsImportant=true, TaskList = new TaskList() },
+				new UserTask() { Text = "mock3", IsImportant=true, TaskList = new TaskList() }
+			}.AsEnumerable());
+		}
+	}
+}
